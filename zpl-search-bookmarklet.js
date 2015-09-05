46bea7dc8d5ff5c4c7c12b20ed56ea5e70c509a8
Pavol Fodor <pfodo@users.noreply.github.com>
2015-09-06 01:06:42 +0200

    Create zpl-search-bookmarklet.js

---
+++ zpl-search-bookmarklet.js
@@ -0,0 +1,1 @@
+javascript: q = '' + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text);if (!q) q = prompt('Hledej zákon:', '');if (q != null) location = 'http://www.zakonyprolidi.cz/hledani?text=' + escape(q).replace(/ /g, '+') + '';void 0
